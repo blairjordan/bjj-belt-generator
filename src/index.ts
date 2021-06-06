59f035756e30d10ef52abba1773bd2d572578b04
Blair <blair.j.jordan@gmail.com>
2021-06-07 00:48:08 +1000

    Unique belt noise.

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,21 @@ import { exec } from 'child_process';
 import path from 'path';
 import yargs, { Argv } from 'yargs';
 
+const COLORS = {
+  purple: '#6a0dad',
+  blue: '#0056c7',
+  brown: '#88442c',
+  black: '#0e0e0e',
+  red: '#da0000'
+};
+
 interface Args {
   color: string;
   name: string;
   instructor: string;
 }
+
+const hash = (input: string) => input.split('').reduce((a,b) =>{ a=( (a<<5) - a) + b.charCodeAt(0); return a&a }, 0);
 
 const argv = yargs.command('serve', 'Start the server.', (yargs: Argv) =>
   yargs
@@ -28,12 +38,45 @@ const argv = yargs.command('serve', 'Start the server.', (yargs: Argv) =>
 ).argv;
 
 const { color, name, instructor } = argv as Args;
-const OUTPUT_PATH = 'images/output';
-const TEMPLATE_PATH = 'images/belt-templates';
 
-const convert = async (beltTemplateFile: string, name: string, instructor: string, outputFile: string): Promise<any> =>
+const TEMP_PATH = 'images/temp';
+const OUTPUT_PATH = 'images/output';
+
+const SIZE = '600x30';
+
+//convert -size 600x30 xc: -seed 33333 +noise Random -modulate 20,0 noise.png
+//convert belt.png \( noise.png -normalize +level 1,4% \) -compose screen -composite output.png
+
+const generateBelt = async (color: string, outputFile: string): Promise<any> =>
   new Promise((resolve, reject) => {
-    exec(`magick convert ${beltTemplateFile} -background white label:"${name}\nInstructor: ${instructor}" -gravity center -append ${outputFile}`,
+    const stroke = (color !== 'black') ? 'black' : 'red';
+    const cmd = `convert -size ${SIZE} xc:${COLORS[color]} -fill black -stroke '${COLORS[stroke]}' -strokewidth 100 -draw "stroke-linecap square path 'M 120,0 L 120,1'" ${outputFile}`;
+    console.log(cmd);
+    exec(cmd,
+    (error, stdout, stderr) => {
+      if (error)  { reject(error);  }
+      if (stderr) { reject(stderr); }
+      resolve(stdout);
+    });
+  });
+
+const generateNoise = async (seed: number, outputFile: string): Promise<any> =>
+  new Promise((resolve, reject) => {
+    const cmd = `convert -size ${SIZE} xc: -seed ${seed} +noise Random -modulate 20,0 ${outputFile}`;
+    console.log(cmd);
+    exec(cmd,
+    (error, stdout, stderr) => {
+      if (error)  { reject(error);  }
+      if (stderr) { reject(stderr); }
+      resolve(stdout);
+    });
+  });
+
+const generateCompositetempBeltFile = async (beltFile: string, noiseFile: string, outputFile: string): Promise<any> =>
+  new Promise((resolve, reject) => {
+    const cmd = `convert ${beltFile} \\( ${noiseFile} -normalize +level 1,4% \\) -compose screen -composite ${outputFile}`;
+    console.log(cmd);
+    exec(cmd,
     (error, stdout, stderr) => {
       if (error)  { reject(error);  }
       if (stderr) { reject(stderr); }
@@ -45,14 +88,18 @@ const convert = async (beltTemplateFile: string, name: string, instructor: strin
   if (!(color && name && instructor)) {
     console.error('Required: color, name, instructor');
   } else {
-    const beltTemplateFile = path.join(TEMPLATE_PATH, `${color.toLowerCase()}-belt.svg`);
-    const outputFile = path.join(OUTPUT_PATH, `${color.toLowerCase()}-belt.png`);
-    console.info(`Name: ${name}\nBelt: ${color}\nInstructor: ${instructor}\nOutput file: ${outputFile}`);
+    const tempBeltFile = path.join(TEMP_PATH,  `belt.png`);
+    const tempNoiseFile = path.join(TEMP_PATH, `noise.png`);
+    const outputFile = path.join(OUTPUT_PATH, `belt-composite.png`);
+    
     try {
-      const stdout = await convert(beltTemplateFile, name, instructor, outputFile);
-      console.log(stdout);
-    } catch (error) {
+      await generateBelt(color, tempBeltFile);
+      await generateNoise(hash(`${name} ${color} ${instructor}`), tempNoiseFile);
+      await generateCompositetempBeltFile(tempBeltFile, tempNoiseFile, outputFile);
+      
+  } catch (error) {
       console.error('An error occured', error);
     }
   }
 })();
+
